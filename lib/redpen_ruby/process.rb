bda8574ad593d2bbe0f2b6c226f06ec1f43ab550
Akino Murasame <akino.murasame@gmail.com>
2014-12-27 13:30:30 +0900

    rename redpen_ruby_bin -> redpen_bin

--- lib/redpen_ruby/process.rb
+++ lib/redpen_ruby/process.rb
@@ -1,8 +1,8 @@
 module RedpenRuby
   class Process
     def redpen_ruby(config, target)
-      redpen_ruby_bin = File.expand_path(File.dirname(__FILE__) + '/../redpen-cli-1.0/bin/redpen')
-      format_redpen_ruby_output(`#{redpen_ruby_bin} -c #{config} #{target} 2>&1`)
+      redpen_bin = File.expand_path(File.dirname(__FILE__) + '/../redpen-cli-1.0/bin/redpen')
+      format_redpen_ruby_output(`#{redpen_bin} -c #{config} #{target} 2>&1`)
     end
 
     def format_redpen_ruby_output(message)
