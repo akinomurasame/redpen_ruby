804000ed6bc643115204264de0ea27bd8520f5e8
Akino Murasame <akino.murasame@gmail.com>
2014-12-27 13:31:29 +0900

    Add format_message_spec.rb

---
+++ spec/redpen_ruby/format_message_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+module RedpenRuby
+  describe RedpenRuby do
+    it 'include "ValidationError" should be false' do
+      message = FormatMessage.new("sampledoc-en.md:2: ValidationError[SentenceLength], 文長が最大値 \"215\" を超えています")
+      result = message.valid?
+      expect(result).to be false
+    end
+  end
+end
