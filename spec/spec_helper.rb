7efb33bef37ec1f5e95fff951e7c8397a7f7beef
Akino Murasame <akino.murasame@gmail.com>
2014-12-29 06:59:20 +0900

    Fix spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,5 +2,10 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'redpen_ruby'
 require 'coveralls'
 require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-Coveralls.wear!
+
+formatters = [SimpleCov::Formatter::HTMLFormatter]
+
+formatters << Coveralls::SimpleCov::Formatter
+formatters << CodeClimate::TestReporter::Formatter
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[*formatters]
