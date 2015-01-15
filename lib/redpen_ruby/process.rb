c3de6e32f38653ed862c0ca3d45fbed4b09588e9
Akino Murasame <akino.murasame@gmail.com>
2015-01-15 14:56:20 +0900

    Update RedPen to v1.1

--- lib/redpen_ruby/process.rb
+++ lib/redpen_ruby/process.rb
@@ -1,7 +1,7 @@
 module RedpenRuby
   class Process
     def redpen_ruby(config, target)
-      redpen_bin = File.expand_path(File.dirname(__FILE__) + '/../redpen-cli-1.0/bin/redpen')
+      redpen_bin = File.expand_path(File.dirname(__FILE__) + '/../redpen-cli-1.1/bin/redpen')
       format_redpen_ruby_output(`#{redpen_bin} -c #{config} #{target} 2>&1`, `#{redpen_bin} -v`)
     end
 
